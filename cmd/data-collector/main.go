62d125994e624c23ba2cd50fee9c1e51d94b6281
Anton B <antonb@columbus-internet.com>
2017-09-18 00:45:44 +0700

    charts; separated datamodels

--- cmd/data-collector/main.go
+++ cmd/data-collector/main.go
@@ -13,39 +13,6 @@ import (
 	mgo "gopkg.in/mgo.v2"
 )
 
-// TinkoffData ...
-type TinkoffData struct {
-	ResultCode string  `json:"resultCode,omitempty"`
-	Payload    Payload `json:"payload,omitempty"`
-	TrackingID string  `json:"trackingId,omitempty"`
-}
-
-// Payload ..
-type Payload struct {
-	LastUpdate LastUpdate `json:"lastUpdate,omitempty"`
-	Rates      []Rate     `json:"rates,omitempty"`
-}
-
-// LastUpdate ...
-type LastUpdate struct {
-	Milliseconds uint64 `json:"milliseconds,omitempty"`
-}
-
-// Rate ...
-type Rate struct {
-	Category     string   `json:"category,omitempty"`
-	FromCurrency Currency `json:"fromCurrency,omitempty"`
-	ToCurrency   Currency `json:"toCurrency,omitempty"`
-	Buy          float64  `json:"buy,omitempty"`
-	Sell         float64  `json:"sell,omitempty"`
-}
-
-// Currency ...
-type Currency struct {
-	Name string `json:"name,omitempty"`
-	Code int    `json:"code,omitempty"`
-}
-
 func main() {
 
 	loc, _ := time.LoadLocation("Europe/Moscow")
--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"io/ioutil"
 	"log"
 	"net/http"
@@ -33,18 +34,94 @@ func index(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+// Dataset ...
+type Dataset struct {
+	Cols []Col `json:"cols,omitempty"`
+	Rows []Row `json:"rows,omitempty"`
+}
+
+// Col ...
+type Col struct {
+	ID      string            `json:"id,omitempty"`
+	Label   string            `json:"label,omitempty"`
+	Type    string            `json:"type,omitempty"`
+	Pattern string            `json:"pattern,omitempty"`
+	P       map[string]string `json:"p,omitempty"`
+	Role    string            `json:"role,omitempty"`
+}
+
+// Row ...
+type Row struct {
+	C []Cell `json:"c,omitempty"`
+}
+
+// Cell ...
+type Cell struct {
+	V interface{}       `json:"v,omitempty"`
+	F string            `json:"f,omitempty"`
+	P map[string]string `json:"p,omitempty"`
+}
+
 func getData(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte(`{
-		"cols": [
-			  {"id":"","label":"Topping","pattern":"","type":"string"},
-			  {"id":"","label":"Slices","pattern":"","type":"number"}
-			],
-		"rows": [
-			  {"c":[{"v":"Mushrooms","f":null},{"v":3,"f":null}]},
-			  {"c":[{"v":"Onions","f":null},{"v":1,"f":null}]},
-			  {"c":[{"v":"Olives","f":null},{"v":1,"f":null}]},
-			  {"c":[{"v":"Zucchini","f":null},{"v":1,"f":null}]},
-			  {"c":[{"v":"Pepperoni","f":null},{"v":2,"f":null}]}
-			]
-	  }`))
+
+	data := Dataset{}
+	data.Cols = []Col{
+		Col{Label: "Day", Type: "number"},
+		Col{Label: "Guardians of the Galaxy", Type: "number"},
+		Col{Type: "string", P: map[string]string{"role": "annotation"}},
+		Col{Type: "string", P: map[string]string{"role": "annotationText"}},
+	}
+	data.Rows = []Row{
+		Row{
+			C: []Cell{
+				Cell{V: 1},
+				Cell{V: 37.8},
+				Cell{V: "37.8"},
+				Cell{V: "37.8"},
+			},
+		},
+		Row{
+			C: []Cell{
+				Cell{V: 2},
+				Cell{V: 30.9},
+				Cell{V: "30.9"},
+				Cell{V: "30.9"},
+			},
+		},
+		Row{
+			C: []Cell{
+				Cell{V: 3},
+				Cell{V: 25.4},
+				Cell{V: "25.4"},
+				Cell{V: "25.4"},
+			},
+		},
+		Row{
+			C: []Cell{
+				Cell{V: 4},
+				Cell{V: 11.7},
+				Cell{V: "11.7"},
+				Cell{V: "11.7"},
+			},
+		},
+		Row{
+			C: []Cell{
+				Cell{V: 5},
+				Cell{V: 11.9},
+				Cell{V: "11.9"},
+				Cell{V: "11.9"},
+			},
+		},
+		Row{
+			C: []Cell{
+				Cell{V: 6},
+				Cell{V: 8.8},
+				Cell{V: "8.8"},
+				Cell{V: "8.8"},
+			},
+		},
+	}
+
+	d, _ := json.Marshal(data)
+	w.Write(d)
 }
