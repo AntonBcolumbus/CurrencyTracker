905ce38a8e72fc20cfb776a423d73712ddd6125f
Anton B <antonb@columbus-internet.com>
2017-09-17 22:57:51 +0700

    json chart

--- main.go
+++ main.go
@@ -17,6 +17,7 @@ func main() {
 
 	router := mux.NewRouter().StrictSlash(true)
 	router.Path("/").Name("index").HandlerFunc(index)
+	router.Path("/getData").Name("index").HandlerFunc(getData)
 
 	log.Fatal(http.ListenAndServe(":"+port, router))
 }
@@ -30,4 +31,20 @@ func index(w http.ResponseWriter, r *http.Request) {
 	if err != nil {
 		w.Write([]byte(err.Error()))
 	}
+}
+
+func getData(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte(`{
+		"cols": [
+			  {"id":"","label":"Topping","pattern":"","type":"string"},
+			  {"id":"","label":"Slices","pattern":"","type":"number"}
+			],
+		"rows": [
+			  {"c":[{"v":"Mushrooms","f":null},{"v":3,"f":null}]},
+			  {"c":[{"v":"Onions","f":null},{"v":1,"f":null}]},
+			  {"c":[{"v":"Olives","f":null},{"v":1,"f":null}]},
+			  {"c":[{"v":"Zucchini","f":null},{"v":1,"f":null}]},
+			  {"c":[{"v":"Pepperoni","f":null},{"v":2,"f":null}]}
+			]
+	  }`))
 }
