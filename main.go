53b5eec2b8c651bc1b01b8b597f51e0be1490f93
Anton B <antonb@columbus-internet.com>
2017-09-17 22:12:05 +0700

    removing records older than 3 days

--- main.go
+++ main.go
@@ -6,6 +6,9 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"time"
+
+	"gopkg.in/mgo.v2/bson"
 
 	mgo "gopkg.in/mgo.v2"
 )
@@ -44,6 +47,18 @@ type Currency struct {
 }
 
 func main() {
+
+	loc, _ := time.LoadLocation("Europe/Moscow")
+	now := time.Now().In(loc)
+
+	if now.Weekday() == time.Sunday ||
+		now.Weekday() == time.Saturday ||
+		now.Hour() < 10 ||
+		(now.Hour() > 18 && now.Minute() > 29) {
+		log.Printf("skipping due to date/time: %s", now.Format("Mon Jan 2 15:04:05 MST 2006"))
+		return
+	}
+
 	url := os.Getenv("MONGODB_URI")
 	if url == "" {
 		url = "mongodb://heroku_k99bcr9h:vo2e0n2drkk3do41t2q9lvh6av@ds141024.mlab.com:41024/heroku_k99bcr9h"
@@ -76,5 +91,11 @@ func main() {
 	err = c.Insert(data)
 	if err != nil {
 		log.Fatalf("mongo insert failed: %s", err.Error())
+	}
+
+	unixTime := now.UTC().Add(time.Hour*24*-3).UnixNano() / int64(time.Millisecond)
+	err = c.Remove(bson.M{"payload.lastupdate.milliseconds": bson.M{"$lt": unixTime}})
+	if err != nil && err != mgo.ErrNotFound {
+		log.Fatalf("mongo remove failed: %s", err.Error())
 	}
 }
